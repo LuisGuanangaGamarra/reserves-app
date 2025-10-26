c14baae28ce6012cdef5764c54aad0633ce0ac81
Luis Guananga <luisgamarra_97@hotmail.com>
2025-10-26 00:47:09 -0500

    add suport memory sqlite

--- back-node/src/infrastructure/di/typeorm.module.ts
+++ back-node/src/infrastructure/di/typeorm.module.ts
@@ -8,7 +8,7 @@ import { TokenDataSource } from '../persistence/typeorm/data-source';
 const isSqlite = process.env.DB_TYPE === 'sqlite';
 
 export const TypeOrmModule = new ContainerModule(async (options: ContainerModuleLoadOptions) => {
-    const ds = isSqlite ? SqLiteDataSource : AppDataSource;
+    const ds = isSqlite ? SqLiteDataSource() : AppDataSource;
 
     if (!ds.isInitialized) {
         await ds.initialize();
--- back-node/src/infrastructure/persistence/typeorm/sqlite-data-source.ts
+++ back-node/src/infrastructure/persistence/typeorm/sqlite-data-source.ts
@@ -1,15 +1,17 @@
 import { DataSource } from "typeorm";
 import path from "path";
 
-export const SqLiteDataSource = new DataSource({
-    type: 'better-sqlite3',
-    database: ':memory:',
-    synchronize: false,
-    logging: ["error"],
-    logger: 'formatted-console',
-    entities: [
-        path.resolve(__dirname, `./entities/*.orm-entity{.ts,.js}`),
-    ],
-    migrations: [ path.resolve(__dirname, `./migrations-sqlite/*{.ts,.js}`)],
-    migrationsTableName: 'migrations',
-})
+export const SqLiteDataSource = () => (
+    new DataSource({
+        type: 'better-sqlite3',
+        database: ':memory:',
+        synchronize: false,
+        logging: ["error"],
+        logger: 'formatted-console',
+        entities: [
+            path.resolve(__dirname, `./entities/*.orm-entity{.ts,.js}`),
+        ],
+        migrations: [ path.resolve(__dirname, `./migrations-sqlite/*{.ts,.js}`)],
+        migrationsTableName: 'migrations',
+    })
+)
